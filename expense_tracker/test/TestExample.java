9463e2c4ae9880210705cf999dfe05a7a56887ab
pranoydev98 <pranoydev98@gmail.com>
2023-11-14 13:29:58 -0500

    Test Case 1

--- expense_tracker/test/TestExample.java
+++ expense_tracker/test/TestExample.java
@@ -2,31 +2,45 @@
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
-
+import static org.junit.Assert.assertFalse;
 import java.util.Date;
 import java.util.List;
-import java.text.ParseException;
 
+import javax.swing.text.View;
+import java.text.ParseException;
 import org.junit.Before;
 import org.junit.Test;
-
+import org.omg.CORBA.DynAnyPackage.Invalid;
 import controller.ExpenseTrackerController;
 import model.ExpenseTrackerModel;
 import model.Transaction;
+import model.Filter.AmountFilter;
+import model.Filter.CategoryFilter;
 import view.ExpenseTrackerView;
-
+import java.awt.*;
+import javax.swing.*;
+import static org.junit.Assert.*;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableModel;
+import java.awt.event.ActionListener;
+import java.text.NumberFormat;
 
 public class TestExample {
   
   private ExpenseTrackerModel model;
   private ExpenseTrackerView view;
   private ExpenseTrackerController controller;
+  private AmountFilter amountFilter;
+  private CategoryFilter categoryFilter;
+  private JOptionPane jOptionPane;
 
   @Before
   public void setup() {
     model = new ExpenseTrackerModel();
     view = new ExpenseTrackerView();
     controller = new ExpenseTrackerController(model, view);
+    jOptionPane = new JOptionPane();
+    ExpenseTrackerApp.initialize(view, controller, jOptionPane);
   }
 
     public double getTotalCost() {
@@ -58,50 +72,50 @@ public class TestExample {
         assertTrue(nowDate.getTime() - transactionDate.getTime() < 60000);
     }
 
-
+    //1 -- Existing
     @Test
     public void testAddTransaction() {
         // Pre-condition: List of transactions is empty
         assertEquals(0, model.getTransactions().size());
     
         // Perform the action: Add a transaction
-	double amount = 50.0;
-	String category = "food";
+	    double amount = 50.0;
+	    String category = "food";
         assertTrue(controller.addTransaction(amount, category));
     
         // Post-condition: List of transactions contains only
-	//                 the added transaction	
+	    // The added transaction	
         assertEquals(1, model.getTransactions().size());
     
         // Check the contents of the list
-	Transaction firstTransaction = model.getTransactions().get(0);
-	checkTransaction(amount, category, firstTransaction);
+	    Transaction firstTransaction = model.getTransactions().get(0);
+	    checkTransaction(amount, category, firstTransaction);
 	
-	// Check the total amount
+	    // Check the total amount
         assertEquals(amount, getTotalCost(), 0.01);
     }
 
-
+    //2 -- Existing
     @Test
     public void testRemoveTransaction() {
         // Pre-condition: List of transactions is empty
         assertEquals(0, model.getTransactions().size());
     
         // Perform the action: Add and remove a transaction
-	double amount = 50.0;
-	String category = "food";
+	    double amount = 50.0;
+	    String category = "food";
         Transaction addedTransaction = new Transaction(amount, category);
         model.addTransaction(addedTransaction);
     
         // Pre-condition: List of transactions contains only
-	//                the added transaction
+	    // The added transaction
         assertEquals(1, model.getTransactions().size());
-	Transaction firstTransaction = model.getTransactions().get(0);
-	checkTransaction(amount, category, firstTransaction);
+	    Transaction firstTransaction = model.getTransactions().get(0);
+	    checkTransaction(amount, category, firstTransaction);
 
-	assertEquals(amount, getTotalCost(), 0.01);
+	    assertEquals(amount, getTotalCost(), 0.01);
 	
-	// Perform the action: Remove the transaction
+	    // Perform the action: Remove the transaction
         model.removeTransaction(addedTransaction);
     
         // Post-condition: List of transactions is empty
@@ -112,5 +126,30 @@ public class TestExample {
         double totalCost = getTotalCost();
         assertEquals(0.00, totalCost, 0.01);
     }
+
+    //New Test Cases
     
+    //1
+    @Test
+    public void testAddTransactionSucceedsView() {
+
+        // Pre-condition: List of transactions is empty
+        List<Transaction> transactions = model.getTransactions();
+        assertEquals(0, transactions.size());
+
+        // Perform the action: Add a transaction
+        double amount = 100.0;
+        String category = "bills";
+        assertTrue(controller.addTransaction(amount, category));
+
+        // Post-condition: List of transactions contains only : 1 transaction row and 1 sum total row
+        assertEquals(2, view.getTableModel().getRowCount());
+        assertEquals(1, (int)view.getTableModel().getValueAt(0, 0));
+        assertEquals(amount, (double)view.getTableModel().getValueAt(0, 1), 0.01);
+        assertEquals(category, view.getTableModel().getValueAt(0, 2));
+
+        // Check the total amount
+        assertEquals(amount, (double)view.getTableModel().getValueAt(1, 3), 0.01);
+    }
+
 }
